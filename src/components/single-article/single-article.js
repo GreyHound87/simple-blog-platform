7f2aea33cba259f7610585c6f29f50114c37edb0
Sergei Smirnov <sasmirnov87@gmail.com>
2023-11-30 15:51:02 +0300

    deleted actions.js ...

--- src/components/single-article/single-article.js
+++ src/components/single-article/single-article.js
@@ -8,40 +8,20 @@ import ResIcon from '../res-icon/res-icon'
 
 const { Meta } = Card
 
-const article = {
-  slug: 'posle-pravok-5thvca',
-  title: 'После правок',
-  description: 'Вносим правочки, почитайте, полезно.',
-  body: 'Нужные правки:\n- при создании статьи нет валидации на пробелы\n- отображаюстся пустые теги',
-  createdAt: '2023-11-27T16:00:21.464Z',
-  updatedAt: '2023-11-29T10:34:08.089Z',
-  tagList: ['Нет пустым тэгам', 'Правки'],
-  favorited: false,
-  favoritesCount: 4,
-  author: {
-    username: 'krabster',
-    image: 'https://android-obzor.com/wp-content/uploads/2022/03/a-3.jpg',
-    following: false,
-  },
-}
-
-function SingleArticle() {
+function SingleArticle(article) {
+  const { title, favoritesCount, tagList, createdAt, description, body, author } = { article }
   return (
     <Card
       title={
         <>
-          <ArticleTitle
-            title={article.title}
-            favoritesCount={article.favoritesCount}
-            authorUsername={article.author.username}
-          />
-          <ArticleDescription tagList={article.tagList} createdAt={article.createdAt} />
+          <ArticleTitle title={title} favoritesCount={favoritesCount} authorUsername={author.username} />
+          <ArticleDescription tagList={tagList} createdAt={createdAt} />
         </>
       }
-      extra={<Avatar src={article.author.image} size={46} icon={<ResIcon />} alt="Author Avatar" />}
+      extra={<Avatar src={author.image} size={46} icon={<ResIcon />} alt="Author Avatar" />}
     >
-      <Meta description={article.description} />
-      <ReactMarkdown>{article.body}</ReactMarkdown>
+      <Meta description={description} />
+      <ReactMarkdown>{body}</ReactMarkdown>
     </Card>
   )
 }
