453b87bf3b2d3c5198f802212f70e6a36f04931c
Sergei Smirnov <sasmirnov87@gmail.com>
2023-12-03 23:00:33 +0300

    header - extra + avatar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,49 +1,60 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Button } from 'antd'
+import { Button, Avatar } from 'antd'
 import { PageHeader } from '@ant-design/pro-layout'
 import { Link } from 'react-router-dom'
+
+import ResIcon from '../res-icon/res-icon'
 import './header.scss'
 
 function Header() {
   const user = useSelector((state) => state.auth.user)
+  let extraContent
+
+  if (user) {
+    console.log(user)
+    extraContent = (
+      <>
+        <Link to="/new-article" key="create">
+          <Button type="text" className="header_btn--create">
+            Create article
+          </Button>
+        </Link>
+        <Link to="/profile" key="profile">
+          <Button type="text" className="header_btn--profile">
+            {user.user.username}
+            <Avatar src={user.user.image} icon={<ResIcon />} size={46} alt="Author Avatar" />
+          </Button>
+        </Link>
+        <Link to="/" key="out">
+          <Button type="text" className="header_btn--log-out">
+            Log Out
+          </Button>
+        </Link>
+      </>
+    )
+  } else {
+    extraContent = (
+      <>
+        <Link to="/sign-in" key="in">
+          <Button type="text" className="header_btn--sign-in">
+            Sign In
+          </Button>
+        </Link>
+        <Link to="/sign-up" key="up">
+          <Button type="text" className="header_btn--sign-up">
+            Sign Up
+          </Button>
+        </Link>
+      </>
+    )
+  }
   return (
     <PageHeader
       className="site-page-header"
       ghost={false}
       title={<Link to="/">Realworld Blog</Link>}
-      extra={
-        user
-          ? [
-              <Link to="/new-article" key="create">
-                <Button type="text" className="header_btn--create">
-                  Create article
-                </Button>
-              </Link>,
-              <Link to="/profile" key="profile">
-              <Button type="text" className="header_btn--profile">
-                {user.username}
-              </Button>
-              </Link>,
-              <Link to="/" key="out">
-                <Button type="text" className="header_btn--log-out">
-                  Log Out
-                </Button>
-              </Link>,
-            ]
-          : [
-              <Link to="/sign-in" key="in">
-                <Button type="text" className="header_btn--sign-in">
-                  Sign In
-                </Button>
-              </Link>,
-              <Link to="/sign-up" key="up">
-                <Button type="text" className="header_btn--sign-up">
-                  Sign Up
-                </Button>
-              </Link>,
-            ]
-      }
+      extra={extraContent}
     />
   )
 }
