9f633c975f167678011aed5cfb4252266787a821
Sergei Smirnov <sasmirnov87@gmail.com>
2023-12-16 02:31:28 +0300

    del links from hd-btns

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Button, Avatar, Typography } from 'antd'
-import { Link } from 'react-router-dom'
+import { Button, Avatar } from 'antd'
+import { Link, useHistory } from 'react-router-dom'
 
 import { logoutUser } from '../../redux/slices/authSlice'
 import ResIcon from '../res-icon/res-icon'
@@ -9,46 +9,58 @@ import './header.scss'
 
 function Header() {
   const dispatch = useDispatch()
+  const history = useHistory()
   const user = useSelector((state) => state.auth.user)
   let extraContent
 
   const handleLogout = () => {
     localStorage.removeItem('authToken')
     dispatch(logoutUser())
+    history.push('/')
+  }
+
+  const handleCreateArticle = () => {
+    history.push('/new-article')
+  }
+
+  const handleSignIn = () => {
+    history.push('/sign-in')
+  }
+
+  const handleSignUp = () => {
+    history.push('/sign-up')
   }
 
   if (user) {
     extraContent = (
       <>
-        {/* <Link to="/new-article"> */}
-        <Button className="header_btn--create">
+        {/* to="/new-article" */}
+        <Button className="header_btn--create" onClick={handleCreateArticle}>
           <span className="btn-txt--sm">Create article</span>
         </Button>
-        {/* </Link> */}
+
         <Link to="/profile" className="edit-profile-link">
           <span className="username">{user.username}</span>
           <Avatar src={user.image} icon={<ResIcon />} size={46} alt="Author Avatar" />
         </Link>
-        {/* <Link to="/"> */}
+        {/* to="/" */}
         <Button size="large" className="header_btn--log-out" onClick={handleLogout}>
           <span className="btn-txt">Log Out</span>
         </Button>
-        {/* </Link> */}
       </>
     )
   } else {
     extraContent = (
       <>
-        {/* <Link to="/sign-in"> */}
-        <Button type="text" size="large" className="header_btn--sign-in">
+        {/* to="/sign-in" */}
+        <Button type="text" size="large" className="header_btn--sign-in" onClick={handleSignIn}>
           <span className="btn-txt">Sign In</span>
         </Button>
-        {/* </Link> */}
-        {/*  <Link to="/sign-up"> */}
-        <Button type="text" size="large" className="header_btn--sign-up">
+
+        {/*  to="/sign-up" */}
+        <Button type="text" size="large" className="header_btn--sign-up" onClick={handleSignUp}>
           <span className="btn-txt">Sign Up</span>
         </Button>
-        {/* </Link> */}
       </>
     )
   }
