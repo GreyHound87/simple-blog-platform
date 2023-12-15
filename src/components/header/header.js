ec76d1a685396505b8f32b03368a75a1fbf9c357
Sergei Smirnov <sasmirnov87@gmail.com>
2023-12-16 01:50:00 +0300

    pageHeader -> usual header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Button, Avatar } from 'antd'
-import { PageHeader } from '@ant-design/pro-layout'
+import { Button, Avatar, Typography } from 'antd'
 import { Link } from 'react-router-dom'
 
 import { logoutUser } from '../../redux/slices/authSlice'
@@ -21,50 +20,48 @@ function Header() {
   if (user) {
     extraContent = (
       <>
-        <Link to="/new-article">
-          <Button className="header_btn--create">
-            <span className="btn-txt--sm">Create article</span>
-          </Button>
-        </Link>
+        {/* <Link to="/new-article"> */}
+        <Button className="header_btn--create">
+          <span className="btn-txt--sm">Create article</span>
+        </Button>
+        {/* </Link> */}
         <Link to="/profile" className="edit-profile-link">
           <span className="username">{user.username}</span>
           <Avatar src={user.image} icon={<ResIcon />} size={46} alt="Author Avatar" />
         </Link>
-        <Link to="/">
-          <Button size="large" className="header_btn--log-out" onClick={handleLogout}>
-            <span className="btn-txt">Log Out</span>
-          </Button>
-        </Link>
+        {/* <Link to="/"> */}
+        <Button size="large" className="header_btn--log-out" onClick={handleLogout}>
+          <span className="btn-txt">Log Out</span>
+        </Button>
+        {/* </Link> */}
       </>
     )
   } else {
     extraContent = (
       <>
-        <Link to="/sign-in">
-          <Button type="text" size="large" className="header_btn--sign-in">
-            <span className="btn-txt">Sign In</span>
-          </Button>
-        </Link>
-        <Link to="/sign-up">
-          <Button type="text" size="large" className="header_btn--sign-up">
-            <span className="btn-txt">Sign Up</span>
-          </Button>
-        </Link>
+        {/* <Link to="/sign-in"> */}
+        <Button type="text" size="large" className="header_btn--sign-in">
+          <span className="btn-txt">Sign In</span>
+        </Button>
+        {/* </Link> */}
+        {/*  <Link to="/sign-up"> */}
+        <Button type="text" size="large" className="header_btn--sign-up">
+          <span className="btn-txt">Sign Up</span>
+        </Button>
+        {/* </Link> */}
       </>
     )
   }
   return (
-    <PageHeader
-      className="site-page-header"
-      ghost={false}
-      title={
+    <header className="site-page-header">
+      <h1>
         <Link to="/" className="title-link">
           Realworld Blog
         </Link>
-      }
-      extra={extraContent}
-    />
+      </h1>
+
+      <div className="page-header-extra">{extraContent}</div>
+    </header>
   )
 }
-
 export default Header
