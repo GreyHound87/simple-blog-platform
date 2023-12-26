0c96f458ca430466bbb3130acf1efa0c390b520f
Sergei Smirnov <sasmirnov87@gmail.com>
2023-12-26 15:00:21 +0300

    + ResIcon.propTypes & .defaultProps

--- src/components/res-icon/res-icon.js
+++ src/components/res-icon/res-icon.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import fallbackImage from '../../assets/avatar.png'
 
-function ResIcon({ width = '46px', height = '46px', borderRadius = '50%' }) {
+function ResIcon({ width, height, borderRadius }) {
   return <img src={fallbackImage} alt="Fallback Icon" style={{ width, height, borderRadius }} />
+}
+
+ResIcon.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+  borderRadius: PropTypes.string,
+}
+
+ResIcon.defaultProps = {
+  width: '46px',
+  height: '46px',
+  borderRadius: '50%',
 }
 
 export default ResIcon
