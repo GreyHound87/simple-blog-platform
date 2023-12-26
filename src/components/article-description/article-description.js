2e19d6c3c4512764bd353634208f88a2ba965972
Sergei Smirnov <sasmirnov87@gmail.com>
2023-12-26 12:24:53 +0300

    + ArticleDescription.propTypes & .defaultProps

--- src/components/article-description/article-description.js
+++ src/components/article-description/article-description.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Tag } from 'antd'
 
 import uniqueTags from '../../helpers/unique-tags'
@@ -19,6 +20,16 @@ function ArticleDescription({ tagList, createdAt, inCard }) {
       <span className={`created-date ${inCard ? 'created-date--in-card' : ''}`}>{formatDate(createdAt)}</span>
     </div>
   )
+}
+
+ArticleDescription.propTypes = {
+  tagList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  createdAt: PropTypes.string.isRequired,
+  inCard: PropTypes.bool,
+}
+
+ArticleDescription.defaultProps = {
+  inCard: false,
 }
 
 export default ArticleDescription
