6e965ea8782c65cff19763a3e0fab17079424065
Sergei Smirnov <sasmirnov87@gmail.com>
2023-12-26 11:44:27 +0300

    ArticleTitle.propTypes, defaultProps + fixed title-link from singleArt

--- src/components/article-title/article-title.js
+++ src/components/article-title/article-title.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import { setSelectedArticle } from '../../redux/slices/articlesSlice'
 import ArticleRate from '../article-rate/article-rate'
@@ -25,15 +26,50 @@ function ArticleTitle({ title, favoritesCount, authorUsername, article, slug, fa
   return (
     <div className={`title-wrapper ${inCard ? 'title-wrapper--in-card' : ''}`}>
       <div>
-        <Link to={`/articles/${slug}`} target="_blank" onClick={() => !inCard && handleTitleClick}>
-          <span className={`item-title ${inCard ? 'item-title--in-card' : ''}`}>{textCutter(title, 50)}</span>
-        </Link>
+        {inCard ? (
+          <span className="item-title item-title--in-card">{textCutter(title, 50)}</span>
+        ) : (
+          <Link to={`/articles/${slug}`} target="_blank" onClick={() => handleTitleClick}>
+            <span className="item-title">{textCutter(title, 50)}</span>
+          </Link>
+        )}
+
         <ArticleRate slug={slug} favorited={favorited} onLikeUpdate={handleLikeUpdate} />
         <span className={`rate-count ${inCard ? 'rate-count--in-card' : ''}`}>{currentFavoritesCount}</span>
       </div>
       <span className={`author-name ${inCard ? 'author-name--in-card' : ''}`}>{authorUsername}</span>
     </div>
   )
+}
+
+ArticleTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  favoritesCount: PropTypes.number.isRequired,
+  authorUsername: PropTypes.string.isRequired,
+  article: PropTypes.shape({
+    slug: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    body: PropTypes.string,
+    createdAt: PropTypes.string,
+    updatedAt: PropTypes.string,
+    tagList: PropTypes.arrayOf(PropTypes.string),
+    favorited: PropTypes.bool,
+    favoritesCount: PropTypes.number,
+    author: PropTypes.shape({
+      username: PropTypes.string,
+      image: PropTypes.string,
+      following: PropTypes.bool,
+    }),
+  }),
+  slug: PropTypes.string.isRequired,
+  favorited: PropTypes.bool.isRequired,
+  inCard: PropTypes.bool,
+}
+
+ArticleTitle.defaultProps = {
+  article: {},
+  inCard: false,
 }
 
 export default ArticleTitle
